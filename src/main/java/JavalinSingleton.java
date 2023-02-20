e8028f7b5c1aab4df8e627da65855123a8602698
J Ellis <thelotris3.14tastic@gmail.com>
2023-02-20 18:08:11 +0000

    Javalin

--- src/main/java/JavalinSingleton.java
+++ src/main/java/JavalinSingleton.java
@@ -19,7 +19,7 @@ public class JavalinSingleton {
     
         //write endpoint here-
         app.get("/hello", ctx->{
-            
+            ctx.result("Hello World");
         });
 
         return app;
